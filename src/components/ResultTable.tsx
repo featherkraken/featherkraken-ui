d6b5d869122fb5e0e7e8235c1b61cb75498554a8
Ingo <ikuba@intension.de>
2020-03-13 13:56:59 +0100

    8: Improve look of result table.
    - Display return column with condition
    - Only display return date when round trip
    - Add duration and calendar icons to flight panels

--- src/components/ResultTable.tsx
+++ src/components/ResultTable.tsx
@@ -1,47 +1,51 @@
 import * as React from "react";
-import { Table, Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { Trip } from "../objects/Trip";
 import { FlightPanel } from "./FlightPanel";
 
 export function ResultTable(props: any) {
   if (props.trips) {
     return (
-      <Table className="m-5">
-        <tbody>
-          {props.trips.map((trip: Trip) => {
-            return (
-              <tr key={trip.link}>
-                <td>
-                  <Container>
-                    <Row>
-                      <Col>
-                        <FlightPanel flight={trip.outwardFlight} />
-                      </Col>
-                      <Col>
-                        <FlightPanel flight={trip.returnFlight} />
-                      </Col>
-                      <Col>
-                        <Row>
-                          <h2>{trip.price} €</h2>
-                        </Row>
-                        <Row className="mt-3">
-                          <a
-                            href={trip.link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            <Button variant="success">Book</Button>
-                          </a>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </Container>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      <div>
+        {props.trips.map((trip: Trip) => {
+          return (
+            <Container className="mb-3 border-bottom">
+              <Row>
+                <Col>
+                  <div className="mb-2">
+                    <b>DEPARTURE</b> Duration: {trip.outwardFlight?.duration}
+                  </div>
+                  <FlightPanel flight={trip.outwardFlight} />
+                </Col>
+                {trip.returnFlight ? (
+                  <Col>
+                    <div className="mb-2">
+                      <b>RETURN</b> Duration: {trip.returnFlight.duration}
+                    </div>
+                    <FlightPanel flight={trip.returnFlight} />
+                  </Col>
+                ) : (
+                  ""
+                )}
+                <Col sm={1}>
+                  <Row>
+                    <h2>{trip.price} €</h2>
+                  </Row>
+                  <Row className="mt-3">
+                    <a
+                      href={trip.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button variant="success">Book</Button>
+                    </a>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          );
+        })}
+      </div>
     );
   }
   return <div></div>;
