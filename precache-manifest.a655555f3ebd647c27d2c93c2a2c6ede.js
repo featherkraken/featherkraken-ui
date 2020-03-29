b449a8441971f9a86220d93ebcea8debf5ab48e6
Ingo <ikuba@intension.de>
2020-03-29 12:21:55 +0200

    Updates

---
+++ precache-manifest.a655555f3ebd647c27d2c93c2a2c6ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1138ba1ad02d0f2594a7bda1c437aec",
+    "url": "/featherkraken-ui/index.html"
+  },
+  {
+    "revision": "45cdfc1c18e3fbcc1023",
+    "url": "/featherkraken-ui/static/css/2.661b9127.chunk.css"
+  },
+  {
+    "revision": "de10d0357d25b5987b43",
+    "url": "/featherkraken-ui/static/css/main.08aca1d7.chunk.css"
+  },
+  {
+    "revision": "45cdfc1c18e3fbcc1023",
+    "url": "/featherkraken-ui/static/js/2.e9ad17b3.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/featherkraken-ui/static/js/2.e9ad17b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de10d0357d25b5987b43",
+    "url": "/featherkraken-ui/static/js/main.37b70735.chunk.js"
+  },
+  {
+    "revision": "759d5554a979341ae1e3",
+    "url": "/featherkraken-ui/static/js/runtime-main.bd2c698b.js"
+  }
+]);
